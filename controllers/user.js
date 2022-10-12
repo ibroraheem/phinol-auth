35c9b3115c6809126597dc03dc6f07117507dc2a
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-10-12 15:22:19 +0100

    Wallet update

--- controllers/user.js
+++ controllers/user.js
@@ -10,14 +10,18 @@ const request = require('request');
 const google = async (req, res) => {
     try {
         const { firstName, lastName, email, password } = req.body
-        const user = await User.findOne({ email });
-        const _user = await User.create({ firstName, lastName, email, password, verified: true })
-        const token = jwt.sign({ email: _user.email, firstName: _user.firstName, lastName: _user.lastName }, process.env.JWT_SECRET, {
-            expiresIn: '1h'
-        })
-        res.status(200).json({ message: 'Login Successful', user: _user.email, firstName: _user.firstName, lastName: _user.lastName, token: token })
+        const user = await User.findOne({ email })
         if (user) {
-            return res.status(401).json({ message: 'User not found' });
+            const token = jwt.sign({ email: user.email, firstName: user.firstName, lastName: user.lastName }, process.env.JWT_SECRET, {
+                expiresIn: '1h'
+            })
+            res.status(200).json({ message: 'Login Successful', user: user.email, firstName: user.firstName, lastName: user.lastName, token: token });
+        } else {
+            const _user = await User.create({ firstName, lastName, email, password, verified: true })
+            const token = jwt.sign({ email: _user.email, firstName: _user.firstName, lastName: _user.lastName }, process.env.JWT_SECRET, {
+                expiresIn: '1h'
+            })
+            res.status(200).json({ message: 'Login Successful', user: _user.email, firstName: _user.firstName, lastName: _user.lastName, token: token })
         }
     } catch (error) {
         res.status(500).json({ error: error.message })
