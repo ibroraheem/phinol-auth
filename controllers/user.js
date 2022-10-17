04571824bd18e4c3ee360d72ec899ed195cd94ea
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-10-17 06:51:16 +0100

    SSS

--- controllers/user.js
+++ controllers/user.js
@@ -17,7 +17,7 @@ const google = async (req, res) => {
             res.status(200).json({ message: 'Sign in via google', email: user.email, firstName: user.firstName, lastName: user.lastName, phoneNumber: user.phoneNumber, token, verified: user.verified, addresses: user.addresses, phoneNumber: user.phoneNumber })
         } else {
             await User.create({ email: email, password: password, firstName: firstName, user_id: phoneNumber, lastName: lastName, phoneNumber: phoneNumber, verified: true })
-            const token = jwt.sign({ email }, process.env.JWT_SECRET)
+            const token = jwt.sign({ email: user.email, google: true }, process.env.JWT_SECRET)
 
             res.status(200).json({ message: 'Sign up via google', token, email: user.email, firstName: user.firstName, firstName, lastName: user.lastName, verified: user.verified, addresses: user.addresses, phoneNumber: user.phoneNumber })
         }
