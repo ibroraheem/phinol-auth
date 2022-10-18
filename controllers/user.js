5c24425b29ac02362ff6e68456563fb72de09e9e
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-10-18 12:09:15 +0100

    New push

--- controllers/user.js
+++ controllers/user.js
@@ -19,7 +19,7 @@ const google = async (req, res) => {
         } else {
             await User.create({ email: email, password: password, firstName: firstName, user_id: phoneNumber, lastName: lastName, phoneNumber: phoneNumber, verified: true })
             const token = jwt.sign({ email: user.email, google: true }, process.env.JWT_SECRET)
-
+            console.log({email: user.user.email,})
             res.status(200).json({ message: 'Sign in via google', email: user.user.email, firstName: user.user.firstName, lastName: user.user.lastName, phoneNumber: user.user.phoneNumber, token, verified: user.user.verified, addresses: user.user.addresses, phoneNumber: user.user.phoneNumber }) console.log({ message: 'Sign in via google', email: user.email, firstName: user.firstName, lastName: user.lastName, phoneNumber: user.phoneNumber, token, verified: user.verified, addresses: user.addresses, phoneNumber: user.phoneNumber })
         }
     } catch (error) {
