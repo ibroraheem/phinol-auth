1c1bc293d4df14aa1bb1b5095723958befc84225
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-10-18 12:47:04 +0100

    New push

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.get('/', (req, res) => {
 app.use(cookieParser('secret'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors({
     origin: '*'
 }));
