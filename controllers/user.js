68cc71ce1cee9de78841f18dbc9d1e4eb194d69d
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-10-18 13:11:21 +0100

    New push

--- controllers/user.js
+++ controllers/user.js
@@ -15,13 +15,13 @@ const google = async (req, res) => {
         if (user) {
             const token = jwt.sign({ email: user.user.email, google: true }, process.env.JWT_SECRET)
             res.status(200).json({ message: 'Sign in via google', email: user.user.email, firstName: user.user.firstName, lastName: user.user.lastName, phoneNumber: user.user.phoneNumber, token, verified: user.user.verified, addresses: user.user.addresses, phoneNumber: user.user.phoneNumber, token: token })
-            console.log({ user: user.user.email })
+            console.log({ user: user.user })
         } else {
             await User.create({ email: email, password: password, firstName: firstName, user_id: phoneNumber, lastName: lastName, phoneNumber: phoneNumber, verified: true })
             const token = jwt.sign({ email: user.user.email, google: true }, process.env.JWT_SECRET)
             console.log({ email: user.user.email, })
             res.status(200).json({ message: 'Sign in via google', email: user.user.email, firstName: user.user.firstName, lastName: user.user.lastName, phoneNumber: user.user.phoneNumber, token, verified: user.user.verified, addresses: user.user.addresses, phoneNumber: user.user.phoneNumber, token: token })
-            console.log({ user: user.user.email })
+            console.log({ user: user.user })
         }
     } catch (error) {
         res.status(500).json({ error: error.message })
