9d04d6d782ffad8b6337a45745dd5ea5f2e71566
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-10-28 10:47:40 +0100

    s

--- controllers/user.js
+++ controllers/user.js
@@ -440,6 +440,7 @@ const viewWalletBalance = async (req, res) => {
         });
     } catch (error) {
         res.status(500).json({ error: error.message })
+        console.log(error)
     }
 }
 
