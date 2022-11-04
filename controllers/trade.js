704dbbbf721a96ef8809596128cbcc25c4ef4ca5
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-11-04 11:26:04 +0100

    Trade final

--- controllers/trade.js
+++ controllers/trade.js
@@ -113,7 +113,7 @@ const buy = async (req, res,) => {
                                 })
                             }
 
-                        }, 60000)
+                        }, 45000)
                     })
                 } else {
                     return res.status(400).json(body)
@@ -236,7 +236,7 @@ const sell = async (req, res) => {
                                 })
                             }
 
-                        }, 60000)
+                        }, 45000)
                     })
                 } else {
                     return res.status(400).json(body)
