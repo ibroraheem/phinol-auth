1dcb5514fda63d088dcde58eca58ef2f1c321337
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-11-07 14:29:17 +0100

    Daily Reward added

--- controllers/streak.js
+++ controllers/streak.js
@@ -2,31 +2,57 @@ const User = require('../models/user')
 const Streak = require('../models/streak')
 const jwt = require('jsonwebtoken')
 
-export const reward = async (req, res) => {
+const reward = async (req, res) => {
     const token = req.headers.authorization.split(' ')[1]
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
     const user = await User.findById(decoded.id)
     const streak = await Streak.findOne({ user: user._id })
     if (streak) {
-        if (streak.streak >= 7) {
-            streak.streak = 0
-            streak.lastAction = Date.now()
-            streak.save()
-            if (streak.streak < 6) {
+        if (streak.lastAction.toDateString() === new Date().toDateString()) {
+            return res.status(400).json({ message: 'You have already claimed your reward for today' })
+        }
+        if (streak.lastAction.toDateString() === new Date(new Date().setDate(new Date().getDate() - 1)).toDateString()) {
+            if (streak.streak >= 7) {
+                streak.streak = 0
+                streak.lastAction = new Date()
+                streak.save()
+                user.phinBalance += 3
+                user.save()
+                return res.status(200).json({ message: 'You have claimed your reward for today' })
+            } else if (streak.streak < 6) {
+                streak.streak += 1
+                streak.lastAction = new Date()
+                streak.save()
                 user.phinBalance += 1
                 user.save()
-                res.status(200).json({ message: 'You have been rewarded with 100 PHIN' })
-            } else if (streak.streak === 6) {
-                user.phinBalance += 2
+                return res.status(200).json({ message: 'You have claimed your reward for today' })
             } else {
-                user.phinBalance += 3
+                streak.streak += 1
+                streak.lastAction = new Date()
+                streak.save()
+                user.phinBalance += 2
+                user.save()
+                return res.status(200).json({ message: 'You have claimed your reward for today' })
             }
-            res.json({ message: `You have been awarded ${user.phinBalance}` })
-        } else {
-            res.json({ message: 'You have not completed the streak' })
         }
-    } else {
-        res.json({ message: 'You have not completed the streak' })
+        if (streak.lastAction.toDateString() !== new Date(new Date().setDate(new Date().getDate() - 1)).toDateString()) {
+            streak.streak = 1
+            streak.lastAction = new Date()
+            streak.save()
+            user.phinBalance += 1
+            user.save()
+            return res.status(200).json({ message: 'You have claimed your reward for today' })
+        }
     }
+    const newStreak = new Streak({
+        user: user._id,
+        streak: 1,
+        lastAction: new Date()
+    })
+    await newStreak.save()
+    user.phinBalance += 1
+    user.save()
+    return res.status(200).json({ message: 'You have claimed your reward for today' })
 }
 
+module.exports = { reward }
