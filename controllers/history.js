67494682ccd5db1063f9f06dd053dc7a70a572c9
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-11-28 20:58:13 +0100

    Refactored Deposit History

--- controllers/history.js
+++ controllers/history.js
@@ -23,7 +23,6 @@ const getHistory = async (req, res) => {
             if (error) throw new Error(error);
             if (body.status === 'success') {
                 const data = body.data
-                console.log(data)
                 return res.status(200).json(history && data)
             }
         });
